6f41717b5e79f74b32b11d0b47670f3d05cfd58a
Beniston Jeyapaul <beniston.j@outlook.com>
2016-06-14 15:16:15 +0530

    Sample dotnet core project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Find the Square value");
+            double a, b;
+            Console.WriteLine("Enter the value to find the square value");
+            if (double.TryParse(Console.ReadLine(), out a)) {
+            b  = a * a;
+            Console.WriteLine("Square value for " + a + " is :"  + b); 
+        } else {
+  //user gave an illegal input. Handle it here.
+            }
+        }
+    }
+}
